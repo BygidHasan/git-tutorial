336d418f3270cf87cddb84c0f9d41e2f1d9c7932
BygidHasan <bygidhasan6@gmail.com>
2021-02-02 16:31:05 +0600

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
-public class Hello {
+public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world from vs code ;)");
+
+        greetings("Bygid Hasan");
+    }
+
+    public static void greetings(String name) {
+        System.out.println("Welcome " + name);
     }
 }
