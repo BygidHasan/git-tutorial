8d81d7dd3e41eeeb1d32a195392caa637f93d76e
BygidHasan <bygidhasan6@gmail.com>
2021-02-02 16:43:52 +0600

    changed greeting method

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world from vs code ;)");
 
-        greetings("Bygid Hasan");
+        greetings("git");
     }
 
     public static void greetings(String name) {
-        System.out.println("Welcome " + name);
+        System.out.println("bye bye " + name);
     }
 }
