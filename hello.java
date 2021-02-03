0a07c396bd115c66e084651f3b60f67b248fd8c7
BygidHasan <bygidhasan6@gmail.com>
2021-02-03 21:26:32 +0600

    delete previous code and added a new method

--- hello.java
+++ hello.java
@@ -5,11 +5,7 @@ public class hello {
     
     }
 
-    public static void message() {
-        System.out.println("hurray, now we are able to contribute on open source");
-    }
-
-    public static int add(int a, int b) {
-        return a+b; 
+    public static int sqr(int a) {
+        return a*a; 
     }
 }
