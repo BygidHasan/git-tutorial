06c6e0c7b82f90aa45aaa6a81920cb5eecdeb36d
BygidHasan <bygidhasan6@gmail.com>
2021-02-03 12:29:30 +0600

    all method removed and added message method

--- hello.java
+++ hello.java
@@ -2,14 +2,10 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world from vs code ;)");
 
-        display();
+    
     }
 
-    public static void display() {
-        System.out.println("hello hello");
-    }
-
-    public static int sqr(int x) {
-        return x*x;
+    public static void message() {
+        System.out.println("hurray, now we are able to contribute on open source");
     }
 }
