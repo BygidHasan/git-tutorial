693b812e060f9c3929afb3523c9da4483fed14b6
BygidHasan <bygidhasan6@gmail.com>
2021-02-03 21:16:49 +0600

    I have added a new file and edited the hello.java

--- hello.java
+++ hello.java
@@ -8,4 +8,8 @@ public class hello {
     public static void message() {
         System.out.println("hurray, now we are able to contribute on open source");
     }
+
+    public static int add(int a, int b) {
+        return a+b; 
+    }
 }
